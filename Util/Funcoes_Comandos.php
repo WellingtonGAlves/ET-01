60f90825977078146dcbee1cb0e25a19c934d2d0
WellingtonGAlves <wellington_999@live.com>
2017-09-20 22:09:30 -0300

    AjustesFuncoes

--- Util/Funcoes_Comandos.php
+++ Util/Funcoes_Comandos.php
@@ -1,9 +1,38 @@
 <?php
     require_once("C:/xampp/htdocs/ET-01/dao/Connection.php");
     
+    function checkAntiRouboPower($imei) {
+        $result = dbExecute("SELECT  `COMANDOS_ALARME_POTENCIA` FROM  `config_et` WHERE  `IMEI` =  '$imei' and `COMANDOS_ALARME_POTENCIA` = 'F1Y1'");
+        print_r($result);
+        if ($result->num_rows == 1) {
+            return "ativarAntiRoubo";
+        }else{            
+            return false;
+        }
+    }
+    
+    function selectIMEI($imei) {
+        $query = "SELECT `IMEI` FROM `config_et` WHERE `IMEI` = '$imei'";
+        if ($query!=0){
+            return true;
+        }else{            
+            return false;
+        }    
+    }
+    
+    function removeHashtag($param) {
+        $posicaoUltimoCaracter = strlen ($param[2]); 
+        $param[2] = substr($param[2], 0, $posicaoUltimoCaracter-1);
+        return $param;
+}
     
     function convertArrayToString($param) {//converte array para string
        $string = implode(",", $param); 
+        return $string;
+    }
+    
+    function convertStringToArray($param) {//converte string para array
+       $string = explode(",", $param); 
         return $string;
     }
     
@@ -15,8 +44,8 @@
     function cadastrarIMEI($imei) {
 //        echo '<br>entrou para cadastrar';
         //cadastrando com informações default
-        $query_insert = dbExecute("INSERT INTO `config_et`(`IMEI`,`TOLERANCIA`, `RASTREAMENTO_AO_VIVO`, `INTERVALO`, `CONTAGEM`, `SERVER_IP`, `UDP_PORTA`, `CORRE`, `CONTROLE_ALARME_POTENCIA`, `COMANDOS_ALARME_POTENCIA`, `CERCA`, `LIMITE_VELOCIDADE`, `SAIR_SUSPENDER`, `ENTRAR_SUSPENDER`, `SENSIBILIDADE`, `INTERVALO_TRANS_DIRIGINDO`, `INTERVALO_TRANS_PARADO`, `MODO_ECONOMICO`, `NUMERO_PRINCIPAL`, `FUSO_HORARIO`, `LINGUAGEM`, `SMS`, `RESTART`, `STATUS_DISPOSITIVO`, `ATIVO`) "
-                . "VALUES ($imei,null,'GZ','0005','0001','179.184.59.144','D431','1',TRUE,'F1Y2',FALSE,NULL,'00F0','00F0','00','05','0A','01',NULL,'3.0','01','01',FALSE,'Aguardando resposta',true)");
+        $query_insert = dbExecute("INSERT INTO `config_et`(`IMEI`,`TOLERANCIA`, `RASTREAMENTO_AO_VIVO`, `INTERVALO`, `CONTAGEM`, `SERVER_IP`, `UDP_PORTA`, `CORRE`, `COMANDOS_ALARME_POTENCIA`, `CERCA`, `LIMITE_VELOCIDADE`, `SAIR_SUSPENDER`, `ENTRAR_SUSPENDER`, `SENSIBILIDADE`, `INTERVALO_TRANS_DIRIGINDO`, `INTERVALO_TRANS_PARADO`, `MODO_ECONOMICO`, `NUMERO_PRINCIPAL`, `FUSO_HORARIO`, `LINGUAGEM`, `SMS`, `RESTART`, `STATUS_DISPOSITIVO`, `ATIVO`) "
+                . "VALUES ($imei,null,'GZ','0005','0001','179.184.59.144','D431','1','F1Y2',FALSE,NULL,'00F0','00F0','00','05','0A','01',NULL,'3.0','01','01',FALSE,'Aguardando resposta',true)");
         
 //        print_r("_".$query_insert."_");
         if(($query_insert)==1){
@@ -24,7 +53,7 @@
             return true;
         }else{
 //            echo "falha ao cadastrar";            
-            false;
+            return false;
         }
     }
     function salvarLocalAtual($msg){
@@ -56,27 +85,52 @@
 //            echo '<br>não atualizou';   
             return false;//nao atualizou
         }
-    }    
+    }
+
+    function updateNumeroMaster($parts_msg) {
+        $result = dbExecute("UPDATE  `config_et` SET  `NUMERO_PRINCIPAL` =  '$parts_msg[3]' WHERE  `IMEI` =  '$parts_msg[1]'");
+        if($result== 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+    function updateRastreador($parts_msg) {
+        $result = dbExecute("UPDATE  `config_et` SET  `NUMERO_PRINCIPAL` =  '$parts_msg[3]' WHERE  `IMEI` =  '$parts_msg[1]'");
+        if($result== 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+    
+    
     function lerMensagem($msg){
     //*ET,SN,DW,A/V,YYMMDD,HHMMSS,Latitude,Longitude,Speed,Course,Status,Signal,Power,oil,LC#   
-    if ($msg=="JAVA") {
-        return "JAVA";
-    }
+    
     $parts_msg = explode(',',$msg);
     $tot_parts = count($parts_msg);
-    echo "<p>_$tot_parts</p>";
+    echo "<p>_Total de partes $tot_parts <-- </p>";
+    
+    $comando = checkAntiRouboPower($parts_msg[1]);
+    echo "<br>daas=$comando";
+    if($comando=="ativarAntiRoubo") {
+        return "ativarAntiRoubo"; // ativar anti-roubo
+    }
     if($tot_parts==16){
         //está mandando sua localização
         $link = dbConnect();
         if($link != null){//verifica se a conexão com o banco
             dbClose($link);//fecha conexao
-            $query = "SELECT `IMEI` FROM `config_et` WHERE `IMEI` = $parts_msg[1]";
-            $resultado_query = dbExecute($query);//verifica se o IMEI está cadastrado
+            
+            $resultado_query = selectIMEI($parts_msg[1]);//verifica se o imei está cadastrado
+                       
             
             //print_r("<br>".$parts_msg[6]."<br>");
            
             
-            if (mysqli_num_rows($resultado_query)==0) {//verifica se o IMEI está cadastrado
+            if ($resultado_query === false) {//verifica se o IMEI está cadastrado
                 echo 'entrou no if para cadastrar';
                 $cadastrou = cadastrarIMEI($parts_msg[1]);//cadastra IMEI
                 if ($cadastrou === true) {   //verifica se cadastrou IMEI
@@ -105,38 +159,45 @@
                 return "dbConnect() falhou";
             }
         }elseif($parts_msg[2]=="RG") {
-            $query = "SELECT `IMEI` FROM `config_et` WHERE `IMEI` = $parts_msg[1]";
-            $resultado_query = dbExecute($query);//verifica se o IMEI está cadastrado
-            
-            if (mysqli_num_rows($resultado_query)==0) {//verifica se o IMEI está cadastrado
+            $resultado_query = selectIMEI($parts_msg[1]);           
+            if ($resultado_query === false) {//verifica se o IMEI está cadastrado
 //                echo 'entrou no if para cadastrar';
                 $cadastrou = cadastrarIMEI($parts_msg[1]);//cadastra IMEI
                 if ($cadastrou === true) {   //verifica se cadastrou IMEI
                     echo "cadastrou";                    
-                    dbExecute("UPDATE  `config_et` SET  `NUMERO_PRINCIPAL` =  '$parts_msg[3]' WHERE  `IMEI` =  '$parts_msg[1]'");                    
-                    return "atualizouNumeroPrincipal";                   
+                    $result = updateNumeroMaster($part);
+                    if ($result==true) {
+                        return "atualizouNumeroPrincipal";
+                    }else{
+                        return "naoAtualizouNumeroPrincipal";
+                    }
+                                       
                 }else{
                     return "naoCadastrou/naoRegistrouNumeroPrincipal";
                 }
             }else{
                 dbConnectClose();
-                $respostaBanco = dbExecute("UPDATE  `config_et` SET  `NUMERO_PRINCIPAL` =  '$parts_msg[3]' WHERE  `IMEI` =  '$parts_msg[1]'");
-                echo "<br>_$respostaBanco _<br>";
-                return "atualizouNumeroPrincipal";
+                $result = updateNumeroMaster($part);
+                if ($result==true) {
+                    return "atualizouNumeroPrincipal";
+                }else{
+                    return "naoAtualizouNumeroPrincipal";
+                }
             }       
-        }
-        if(explode("#", $parts_msg[2])){
-            echo "<p>_explode $parts_msg[2]</p>";
-            $posicaoUltimoCaracter = strlen ($msg[2]); 
-            $parts_msg[2] = substr($parts_msg[2], 0, $posicaoUltimoCaracter-1);
-//            print_r($msg[15]);
-            echo "<p>_explode Retirando $parts_msg[2]</p>";
-            
-        if ($parts_msg[2]=="MQ") {
-            echo "<br>return MQ<br>";
+        }elseif ($parts_msg[2]=="MQ#") {
+            echo "<br>elseif ($parts_msg[2]==MQ<br>";
             return "MQ";
-        }
-        }
+        }else{
+            return "sem tratamento";
+        }        
+//        if(explode("#", $parts_msg[2])){
+//            echo "<p>_explode $parts_msg[2]</p>";
+//             
+//            $parts_msg[2] = removeHashtag($parts_msg);
+////            print_r($msg[15]);
+//            echo "<p>_explode Retirando $parts_msg[2]</p>";
+//            return "removeu hashtag";       
+//        }
     }
     
     
--- controller/Controller_Comandos.php
+++ controller/Controller_Comandos.php
@@ -8,7 +8,7 @@
     $port = 54321;
     $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create
     socket<br>");
-    $result = socket_bind($socket,$host,54000) or die("Could not bind to
+    $result = socket_bind($socket,$host,54321) or die("Could not bind to
     socket<br>");
     $result = socket_listen($socket) or die("Could not set up socket
     listener<br>");
@@ -24,50 +24,86 @@
         while(socket_recv($spawn,$buffer_enviado_rastreador,1024,MSG_WAITALL))
         {
         
-            $msg = trim($buffer_enviado_rastreador); 
-            echo "<p>Rastreador Enviou : ".$msg."</p>";    
+            $msg = trim($buffer_enviado_rastreador);
+            if($msg == "exit"){
+                exit;
+            }
+            echo "<p>Rastreador Enviou : ".$msg."</p>";
+            
+            
             $resultadoComando = lerMensagem($msg);
-            if ($resultadoComando == "atualizouNumeroPrincipal") {
-                $parts[3]="Numero principal registrado na Plataforma"; 
-                $msgConfigurada = arrayToString($parts);//convertendo Array para String
+            $parts_msg = convertStringToArray($msg);
+            
+            //echo "<p>resultadoComando :$resultadoComando___</p>";
+            
+            if($resultadoComando=="fsdsf"){
+            }elseif($resultadoComando == "sem tratamento"){// retirar-----------------------------------------------------------
+                socket_write($spawn,"sem tratamento:"."$msg",strlen("sem tratamento:"."$msg"));//enviaa uma resposta ao tracker
+                socket_close($spawn);
+                echo "<p>Servidor Respondeu :sem tratamento:</p>";
+            }elseif($resultadoComando=="ativarAntiRoubo") {
+                $comando = "*ET,".$parts_msg[1].",FD,F1Y1#";
+                echo "<br>__$comando__<br>";
+                socket_write($spawn,$comando,strlen($comando));//enviaa uma resposta ao tracker
+                //socket_close($spawn);//fecha conexão com o tracker
+//                echo "<p>Resultado do contato :_$resultadoComando_</p>";
+                echo "<p>Servidor Respondeu :".$comando."</p>";
+            }elseif($resultadoComando === "atualizouNumeroPrincipal") {
+                $parts_msg[3]="Numero Master registrado na Plataforma"; 
+                $msgConfigurada = arrayToString($parts_msg);//convertendo Array para String
                 $msgConfigurada = addHashtag($msgConfigurada);
                 echo "<br>mensagem configurada: $msgConfigurada<br>";
                 socket_write($spawn,$msgConfigurada,strlen($msgConfigurada));//enviaa uma resposta ao tracker
                 socket_close($spawn);//fecha conexão com o tracker
-                echo "<p>Resultado do contato : ".$resultadoComando."</p>";        
-            }elseif($resultadoComando == "MQ"){//MQ é o comando para o tracker saber se deve CARREGAR dados        
-                $msgConfigurada = convertArrayToString($parts);//convertendo Array para String
-                //$msgConfigurada .="#";
-                echo "<br>entrou MQ_$msgConfigurada_<br>";
-                socket_write($spawn,$msgConfigurada,strlen($msgConfigurada));
+//                echo "<p>Resultado do contato : ".$resultadoComando."</p>";
+                echo "<p>Servidor Respondeu :".$msgConfigurada."</p>";
+            }elseif($resultadoComando == "atualizouNumeroPrincipal"){
+                $parts_msg[3]="Numero Master registrado na Plataformaaaaaa"; 
+                $msgConfigurada = arrayToString($parts_msg);//convertendo Array para String
+                $msgConfigurada = addHashtag($msgConfigurada);
+                echo "<br>mensagem configurada: $msgConfigurada<br>";
+                socket_write($spawn,$msgConfigurada,strlen($msgConfigurada));//enviaa uma resposta ao tracker
+                socket_close($spawn);//fecha conexão com o tracker
+//                echo "<p>Resultado do contato : ".$resultadoComando."</p>";   
+                echo "<p>Servidor Respondeu :".$msgConfigurada."</p>";
+            }elseif($resultadoComando === "MQ"){//MQ é o comando para o tracker saber se deve CARREGAR dados        
+                socket_write($spawn,$msg,strlen($msg));
                 socket_close($spawn);
-            }elseif ($resultadoComando=="JAVA") {
-                $fala = "fala mano";
-                echo "<br>Vai falar<br>";
-                socket_write($spawn,$fala,strlen($fala));
+                echo "<p>Servidor Respondeu :".$msg."</p>";
             }else{
-                $not = "Não fez nada";
+                
                 //$speed = "*ET,358155100088765,MQ#";
-                socket_write($spawn,$not,strlen($not));
-                socket_close($spawn);                
+                socket_write($spawn,$resultadoComando,strlen($resultadoComando));
+                socket_close($spawn);
+                echo "<p>Servidor Respondeu :".$resultadoComando."</p>";                
             }            
-        }
-        if($msg == "exit"){
-            exit;
         }        
+        $date = fsockopen('udp://pool.ntp.br', 123, $err_no, $err_str, 1);
+        if ($date)
+        {
+            if (fwrite($date, chr(bindec('00'.sprintf('%03d', decbin(3)).'011')).str_repeat(chr(0x0), 39).pack('N', time()).pack("N", 0)))
+            {
+               stream_set_timeout($date, 1);
+               $unpack0 = unpack("N12", fread($date, 48));
+               echo date('Y-m-d H:i:s', $unpack0[7]);
+           }
+           fclose($date);
+        }       
     }
-    socket_close($socket);//encerra servidor
+    
 ?>
 <?php
-$socket = fsockopen('udp://pool.ntp.br', 123, $err_no, $err_str, 1);
-if ($socket)
-{
-    if (fwrite($socket, chr(bindec('00'.sprintf('%03d', decbin(3)).'011')).str_repeat(chr(0x0), 39).pack('N', time()).pack("N", 0)))
+    $date = fsockopen('udp://pool.ntp.br', 123, $err_no, $err_str, 1);
+    if ($date)
     {
-        stream_set_timeout($socket, 1);
-        $unpack0 = unpack("N12", fread($socket, 48));
-        echo date('Y-m-d H:i:s', $unpack0[7]);
+        if (fwrite($date, chr(bindec('00'.sprintf('%03d', decbin(3)).'011')).str_repeat(chr(0x0), 39).pack('N', time()).pack("N", 0)))
+        {
+            stream_set_timeout($date, 1);
+            $unpack0 = unpack("N12", fread($date, 48));
+            echo date('Y-m-d H:i:s', $unpack0[7]);
+        }
+        fclose($date);
     }
-    fclose($socket);
-}
+    print_r($date);
+    socket_close($socket);//encerra servidor
 ?>
